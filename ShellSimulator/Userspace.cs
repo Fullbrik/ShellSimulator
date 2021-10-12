a4923918c14a813633ba1d0b85cd4accfa815b19
Mcp613 <40151628+Fullbrik@users.noreply.github.com>
2021-10-11 20:10:49 -0700

    f

--- ShellSimulator/Userspace.cs
+++ ShellSimulator/Userspace.cs
@@ -1,14 +1,20 @@
 namespace ShellSimulator
 {
-    public class Userspace
-    {
-        public OperatingSystem OS { get; }
+	public class Userspace
+	{
+		public OperatingSystem OS { get; }
+		public string Username { get; }
+		public string HomeFolder { get; }
 
-        public string Username { get; }
+		public string CurrentWorkingDirectory { get; set; } = "~";
 
-        public Userspace(OperatingSystem os, string username, string homeFolder)
-        {
-            OS = os;
-        }
-    }
+		public Userspace(OperatingSystem os, string username, string homeFolder)
+		{
+			OS = os;
+			Username = username;
+			HomeFolder = homeFolder;
+		}
+
+
+	}
 }
