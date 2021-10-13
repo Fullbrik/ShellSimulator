a0d186e9ca120389923a7b38dad187c387962f92
fullbrik <support@fullbrik.com>
2021-10-12 22:39:44 -0700

    More work on user signin stuff

--- ShellSimulator.OS.Simnix/Lib/LibUser.cs
+++ ShellSimulator.OS.Simnix/Lib/LibUser.cs
@@ -1,31 +1,92 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace ShellSimulator.OS.Simnix.Lib
 {
-	public class LibUser : Library
-	{
-		public LibUser(Application application) : base(application)
-		{
-		}
+    public class LibUser : Library
+    {
+        public struct UserData
+        {
+            public string Name { get; }
+            public int UID { get; }
+            public string HomeDirectory { get; }
+            public string Shell { get; }
 
-		public override string Name => "User";
+            public static UserData Parse(string line)
+            {
+                var parts = line.Split(':');
 
-		public bool HasUser(string username)
-		{
+                // Note: /etc/passwd is in the same format as it is on GNU/Linux (I'm specifying GNU because I don't know what android uses).
+                // We don't use all the fields but they have to be there anyway
+                if (parts.Length == 7)
+                {
+                    // Linux format: name:password:UID:GID:GECOS:directory:shell
+                    // We need: name (0), UID (2), (home) directory (5), shell (6)
+                    if (int.TryParse(parts[2], out int uid)) // This param is supposed to be a number, so lets make sure it is.
+                    {
+                        return new UserData(parts[0], uid, parts[5], parts[6]);
+                    }
+                    else
+                    {
+                        throw new System.Exception("UID parameter (2) isn't a integer.");
+                    }
+                }
+                else
+                {
+                    throw new System.Exception("Invalid parameters in file");
+                }
+            }
 
-		}
+            public UserData(string name, int uid, string homeDir, string shell)
+            {
+                Name = name;
+                UID = uid;
+                HomeDirectory = homeDir;
+                Shell = shell;
+            }
+        }
 
-		private string ReadUserDataFile()
-		{
-			// Get the text of the file and close it right away
-			var file = Application.OS.OpenFile("/etc/passwd", Application);
-			string text = file.ReadAllText();
-			file.Close(Application);
+        public LibUser(Application application) : base(application)
+        {
+        }
 
-			return text;
-		}
+        public override string Name => "User";
 
-		private UserData[] ParseUserData(string text)
-		{
+        public bool HasUser(string username)
+        {
+            var users = Loadusers();
 
-		}
-	}
+            // We loop through each user. if we find one that has the right username, we return true right away. Otherwise, we return false
+            foreach (var user in users)
+                if (user.Name == username) return true;
+
+            return false;
+        }
+
+        private UserData[] Loadusers()
+        {
+            string text = ReadUserDataFile();
+            return ParseUserData(text).ToArray();
+        }
+
+        private string ReadUserDataFile()
+        {
+            // Get the text of the file and close it right away
+            var file = Application.OS.OpenFile("/etc/passwd", Application);
+            string text = file.ReadAllText();
+            file.Close(Application);
+
+            return text;
+        }
+
+        private IEnumerable<UserData> ParseUserData(string text)
+        {
+            var lines = text.Split('\n');
+
+            foreach (var line in lines)
+            {
+                yield return UserData.Parse(line);
+            }
+        }
+    }
 }
--- ShellSimulator.OS.Simnix/UserLogin.cs
+++ ShellSimulator.OS.Simnix/UserLogin.cs
@@ -2,24 +2,68 @@ using System.Threading.Tasks;
 
 namespace ShellSimulator.OS.Simnix
 {
-	public class UserLogin : Application
-	{
-		public override string Name => "User Login";
+    public class UserLogin : Application
+    {
+        public override string Name => "User Login";
 
-		public string InputUsername { get; private set; }
-		public string InputPassword { get; private set; }
+        public string InputUsername { get; private set; }
+        public string InputPassword { get; private set; }
 
-		protected override async Task<int> Main(string[] args)
-		{
-			ClearScreen();
+        protected override async Task<int> Main(string[] args)
+        {
+            var libUser = new Lib.LibUser(this);
 
-			PrintF("Username: ");
-			InputUsername = await ReadLine();
+            bool isNextArgAParam = false; // Some options (e.g. --username) request a parameter (e.g. --username [username])
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (isNextArgAParam)
+                {
 
-			PrintF("Password: ");
-			InputPassword = await ReadLine();
+                }
+                else
+                {
+                    switch (args[i])
+                    {
+                        case "-u":
+                        case "-username":
+                            isNextArgAParam = true;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
 
-			return 0;
-		}
-	}
+            if (args.Length == 1)
+            {
+                // I can't think of any yet
+            }
+            else if (args.Length == 2)
+            {
+                switch (args[0])
+                {
+                    // Just verify provided username
+                    case "-u":
+                    case "--username":
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                ClearScreen();
+
+                PrintF("Username: ");
+                InputUsername = await ReadLine();
+
+                PrintF("Password: ");
+                InputPassword = await ReadLine();
+            }
+
+
+
+            return 0;
+        }
+    }
 }
